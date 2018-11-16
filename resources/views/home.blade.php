b6f1169dc90fc9a40413ff6f4cacaa4e70b72401
unknown <omaribanez100@gmail.com>
2018-11-16 14:29:42 -0500

    Commit inicial, con bootstrap incluido

---
+++ resources/views/home.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <base href="{{URL::asset('/')}}" target="_blank">
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/stylehome.css') }}">
+    <link rel="stylesheet" href="{{ url('css/mdb.lite.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/mdb.min') }}">
+    
+   
+    <script src="{{ url('js/jquery-3.3.1.min.js') }}"></script>
+   
+   
+    <title>App Mayathon</title>
+</head>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark indigo">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+          <span class="navbar-text ">
+              <form class="form-inline my-2 my-lg-0 ml-auto">
+                  <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-white btn-md my-2 my-sm-0 ml-3" type="submit">Search</button>
+                </form>
+          </span>
+        </div>
+      </nav>
+</header>
+
+<body>
+  
+    
+    
+    
+    <script src="{{ url('js/ popper.min') }}"></script>
+    <script src="{{ url('js/bootstrap.js') }}"></script>
+    
+</body>
+</html>
